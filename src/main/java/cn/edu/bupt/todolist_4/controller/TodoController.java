b698bd1ee165d7db033fee193d66204652f3ba7f
姝仪 陈 <929946014@qq.com>
2020-12-13 18:03:38 +0800

    加上了登录、登出页面 和 拦截器。

--- src/main/java/cn/edu/bupt/todolist_4/controller/TodoController.java
+++ src/main/java/cn/edu/bupt/todolist_4/controller/TodoController.java
@@ -11,12 +11,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/todo")
+@RequestMapping("/")
 public class TodoController {
     private TodoRepository todoRepository;
 
@@ -25,6 +28,54 @@ public class TodoController {
         this.todoRepository = todoRepository;
     }
 
+    @GetMapping({"/","/login"})
+    String getIndex(Model model) {
+
+        model.addAttribute("today", Calendar.getInstance());
+
+        return "login";
+    }
+    @PostMapping("/login")
+    String post(String username, String password, HttpSession session) throws IOException {
+        System.out.println(username);
+        System.out.println(password);
+
+        if(session.getAttribute("login")==null) {
+            if (username.equals("admin") && password.equals("123456")) {
+//                HttpSession session=request.getSession();
+                session.setAttribute("login", "true!");//若用户名和密码都正确，则将会话的login置为true
+                System.out.println(session.getAttribute("login"));
+                return "redirect:list";
+            } else {
+//            session.setAttribute("login", "false!");
+                System.out.println(session.getAttribute("login"));
+                return "redirect:login";
+            }
+        }
+        else{
+            return "redirect:list";
+        }
+    }
+
+    @GetMapping("/logout")
+    String logout(HttpSession session, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=utf-8");
+
+        if(session.getAttribute("login")==null)
+        {
+
+            System.out.println("please login first.");
+            return "redirect:/login";
+        }
+        else
+        {
+            session.removeAttribute("login");
+            System.out.println("logout successfully!");
+            return "logout";
+        }
+
+//        return "redirect:/login";
+    }
     @GetMapping("/list")
     String list(Model model,
                 @RequestParam(value = "start",defaultValue = "0") Integer start,
@@ -54,14 +105,19 @@ public class TodoController {
         List<Todo> todos = todoRepository.findAll();
 
         System.out.println("控制器端接收到的tel="+tel);
-
-        String result="empty";
-        for (Todo each:todos
-        ) {
-            System.out.println(each.getTel());
-            if(each.getTel().equals(tel)){
-                result="success";
-                break;
+        String result="";
+        if(tel.equals("")){
+            result="empty";
+        }
+        else
+        {
+            for (Todo each:todos
+            ) {
+                System.out.println(each.getTel());
+                if(each.getTel().equals(tel)){
+                    result="success";
+                    break;
+                }
             }
         }
         System.out.println("控制器端result="+result);
